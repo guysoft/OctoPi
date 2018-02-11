f424ac0d685ec6313e77b2c77b46c0bb007e6186
nbelakovski <nbelakovski@users.noreply.github.com>
2018-02-11 09:49:04 -0800

    Removed sudo from vagrant build, it is not necessary

--- src/vagrant/run_vagrant_build.sh
+++ src/vagrant/run_vagrant_build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo vagrant ssh -- -t "sudo /CustomPiOS/nightly_build_scripts/custompios_nightly_build $@"
+vagrant ssh -- -t "sudo /CustomPiOS/nightly_build_scripts/custompios_nightly_build $@"
 
